5cc98c790e153d1c3f5b79cc1b6c9a03aaa7482d
khalad007 <www.arifhossain49919@gmail.com>
2023-12-19 22:28:32 +0600

    Find & Find All

--- web_scraping.py
+++ web_scraping.py
@@ -1,9 +1,17 @@
+# from bs4 import BeautifulSoup
+# import requests
+
+# url = 'https://www.scrapethissite.com/pages/forms/'
+# page = requests.get(url)
+# soup = BeautifulSoup(page.text, 'html.parser')
+
+
+# print(soup.prettify)
+
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.scrapethissite.com/pages/forms/'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
-
-
-print(soup.prettify)
+res = requests.get('https://btst-blush.vercel.app/posts')
+soup = BeautifulSoup(res.text, "html.parser")
+result = soup.find_all("h2")[0]
+print(result)
