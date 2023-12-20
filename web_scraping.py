0e937bf9b4371c7be6dc3f529ee9b36480db673a
khalad007 <www.arifhossain49919@gmail.com>
2023-12-20 23:48:01 +0600

    Testing with if else

--- web_scraping.py
+++ web_scraping.py
@@ -8,10 +8,44 @@
 
 # print(soup.prettify)
 
+# from bs4 import BeautifulSoup
+# import requests
+
+# res = requests.get('https://btst-blush.vercel.app/posts')
+# soup = BeautifulSoup(res.text, "html.parser")
+# result = soup.find_all("h2")[0]
+# print(result)
+
+
+
+
+# from bs4 import BeautifulSoup
+# import requests
+
+# res = requests.get('https://btst-blush.vercel.app/posts')
+
+# soup = BeautifulSoup(res.text, "html.parser")
+# result = soup.find_all("h2")
+# print(result)
+
 from bs4 import BeautifulSoup
 import requests
 
-res = requests.get('https://btst-blush.vercel.app/posts')
-soup = BeautifulSoup(res.text, "html.parser")
-result = soup.find_all("h2")[0]
-print(result)
+url = "https://btst-blush.vercel.app/posts"
+response = requests.get(url)
+
+if response.status_code == 200:
+    responseData = response.content
+else:
+    print('Error')
+    exit()
+
+soup = BeautifulSoup(responseData, 'html.parser')
+
+result = soup.find_all("h2")
+
+for h2 in result:
+    print(h2)
+
+
+
