3ae79000056368412a79fa73446ece110b0df481
khalad007 <www.arifhossain49919@gmail.com>
2023-12-26 15:50:22 +0600

    Wikipedia scraping

--- web_scraping.py
+++ web_scraping.py
@@ -28,24 +28,44 @@
 # result = soup.find_all("h2")
 # print(result)
 
+
+# 333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+
+# from bs4 import BeautifulSoup
+# import requests
+
+# url = "https://btst-blush.vercel.app/posts"
+# response = requests.get(url)
+
+# if response.status_code == 200:
+#     responseData = response.content
+# else:
+#     print('Error')
+#     exit()
+
+# soup = BeautifulSoup(responseData, 'html.parser')
+
+# result = soup.find_all("h2")
+
+# for h2 in result:
+#     print(h2)
+
+# scraping from wikipedia 
+
+
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://btst-blush.vercel.app/posts"
-response = requests.get(url)
 
-if response.status_code == 200:
-    responseData = response.content
-else:
-    print('Error')
-    exit()
+url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
+    
+page = requests.get(url)
 
-soup = BeautifulSoup(responseData, 'html.parser')
+soup = BeautifulSoup(page.text, 'html.parser')
 
-result = soup.find_all("h2")
-
-for h2 in result:
-    print(h2)
+table = soup.find_all('th')
+print(soup.title)
+print(table)
 
 
 
